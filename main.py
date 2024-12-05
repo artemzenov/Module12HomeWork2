f003c6a5028288e7a2afb0a57719c8eb3d9dba3e
Artem Zenov <zenovae1987@gmail.com>
2024-12-05 21:44:02 +0500

    updated code

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ class TournamentTest(TestCase):
     self.runner3 = Runner(name='Ник', speed=3)
     self.distance = 90
     TournamentTest.count += 1
-    self.first_runner = None
-    self.last_runner = None
-    self.result_tournament = None
   
   def _last_runner(self, *runners):
     self.last_runner = runners[0]
@@ -36,27 +33,20 @@ class TournamentTest(TestCase):
         self.first_runner = str(runner)
   
   def test_tournament1(self):
-    tournament = Tournament(self.distance, self.runner1, self.runner3)
-    self.result_tournament = tournament.start()
-    TournamentTest.all_results[TournamentTest.count] = self.result_tournament
-    self._last_runner(self.runner1, self.runner3)
-    self._first_runner(self.runner1, self.runner3)
+    self.list_runners = [self.runner1, self.runner3]
   
   def test_tournament2(self):
-    tournament = Tournament(self.distance, self.runner2, self.runner3)
-    self.result_tournament = tournament.start()
-    TournamentTest.all_results[TournamentTest.count] = self.result_tournament
-    self._last_runner(self.runner2, self.runner3)
-    self._first_runner(self.runner2, self.runner3)
+    self.list_runners = [self.runner2, self.runner3]
   
   def test_tournament3(self):
-    tournament = Tournament(self.distance, self.runner1, self.runner2, self.runner3)
-    self.result_tournament = tournament.start()
-    TournamentTest.all_results[TournamentTest.count] = self.result_tournament
-    self._last_runner(self.runner2, self.runner1, self.runner3)
-    self._first_runner(self.runner2, self.runner1, self.runner3)
+    self.list_runners = [self.runner1, self.runner2, self.runner3]
   
   def tearDown(self):
+    self.tournament = Tournament(self.distance, *self.list_runners)
+    self.result_tournament = self.tournament.start()
+    TournamentTest.all_results[TournamentTest.count] = self.result_tournament
+    self._last_runner(*self.list_runners)
+    self._first_runner(*self.list_runners)
     self.assertTrue(self.result_tournament[max(self.result_tournament)] == self.last_runner)
     self.assertTrue(self.result_tournament[min(self.result_tournament)] == self.first_runner)
 
